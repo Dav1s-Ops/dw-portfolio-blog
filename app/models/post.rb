14a29bc78d771144e75e2241854ee5eafc1908df
Davis Weimer <davisweimer@gmail.com>
2024-10-13 19:58:57 -0600

    Add remaining associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  enum status: { draft: 0, published: 1 }
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  validates :title, :content, presence: true
 end
